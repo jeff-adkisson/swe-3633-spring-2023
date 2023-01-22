d59e1a621ef3fa5dc50ec0520c7b968ccab7686a
Jeff Adkisson <Jeff@HighMatch.com>
2023-01-22 17:09:04 -0500

    FlowFinalNode code demo

---
+++ FlowFinalNode/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+
+namespace FlowFinalNode;
+
+/// <summary>
+/// C# console application to demonstrate the use of flow final node concept.
+/// Demonstrates converting a concurrent activity diagram into threaded code.
+/// </summary>
+public static class Program
+{
+    //number of search threads
+    private const int SearchThreadCount = 3;
+    
+    //random search value we want has a simulated probability greater than this...
+    private const double StopWhenRandomValueIsGreaterThan = 0.98d;
+    
+    //threads, synchronization, counters, and stopwatch
+    private static Thread[]? _solutionSearchThreads;
+    private static double  _solutionFoundValue = Double.NaN;
+    private static readonly object SolutionFoundLock = new object();
+    private static int _overallSearchIterationCount = 0;
+    private static Stopwatch _stopwatch = new Stopwatch();
+    
+    /// <summary>
+    /// Starts <see cref="SearchThreadCount"/> threads to search for a solution.
+    /// The "solution" is simply a random number greater than <see cref="StopWhenRandomValueIsGreaterThan"/>.
+    /// Once a solution is found, the threads join back into the main flow to
+    /// present the solution.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("SOLUTION SEARCH ACTIVITY STARTING");
+        _stopwatch.Start();
+
+        Console.WriteLine("Post Problem Action");
+
+        //start concurrent operation
+        Console.WriteLine($"FORKING {SearchThreadCount} threads to search for solution");
+        
+        //make threads and start them
+        _solutionSearchThreads = new Thread[SearchThreadCount];
+        for (int i = 0; i < SearchThreadCount; i++)
+        {
+            _solutionSearchThreads[i] = new Thread(PerformSearch)
+            {
+                Name = $"{i + 1}: Search Thread {i + 1}"
+            };
+            _solutionSearchThreads[i].Start();
+        }
+
+        //join threads back into main flow
+        foreach (var t in _solutionSearchThreads)
+        {
+            t.Join();
+        }
+        //at this point, the main thread is waiting on all search threads to finish
+
+        //concurrent operation complete, resume main process
+        Console.WriteLine("JOINED, all Search Threads are complete");
+
+        Console.WriteLine($"Select Solution Action: Search Value is: {_solutionFoundValue}");
+        
+        Console.WriteLine("SOLUTION SEARCH ACTIVITY COMPLETE");
+        _stopwatch.Stop();
+        Console.WriteLine($"* Found in {_overallSearchIterationCount} total iterations over {SearchThreadCount} threads.");
+        Console.WriteLine($"* Execution time: {_stopwatch.Elapsed}");
+    }
+
+    /// <summary>
+    /// Loops until a random value greater than <see cref="StopWhenRandomValueIsGreaterThan"/>
+    /// is found. If found, the value is stored in <see cref="_solutionFoundValue"/> and
+    /// execution stops. All threads will be joined back into the main flow when they
+    /// note that <see cref="_solutionFoundValue"/> is no longer <see cref="Double.NaN"/>.
+    /// </summary>
+    private static void PerformSearch()
+    {
+        var rnd = new Random();
+        Console.WriteLine($"{Thread.CurrentThread.Name}: STARTING, SEARCHING FOR A VALUE GREATER THAN {StopWhenRandomValueIsGreaterThan}");
+
+        var iterationCount = 0;
+        double searchValue;
+        bool foundSearchValue;
+        
+        do
+        {
+            lock (SolutionFoundLock) _overallSearchIterationCount += 1;
+            if (double.IsNaN(_solutionFoundValue)) 
+                Thread.Sleep(rnd.Next(250, 1000)); //simulate search effort
+            
+            searchValue = rnd.NextDouble();
+            Console.WriteLine($"{Thread.CurrentThread.Name}: Search iteration {++iterationCount}, " +
+                              $"found: {searchValue}, " +
+                              $"{(IsSearchValueFound(searchValue) ? ":)" : ":(")}");
+            foundSearchValue = IsSearchValueFound(searchValue);
+        } while (double.IsNaN(_solutionFoundValue) && !foundSearchValue);
+
+        lock (SolutionFoundLock)
+        {
+            if (double.IsNaN(_solutionFoundValue) && foundSearchValue)
+            {
+                _solutionFoundValue = searchValue;
+                Console.WriteLine($"{Thread.CurrentThread.Name}: SOLUTION FOUND! {_solutionFoundValue}, I WON! :)");
+            }
+            else
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: SOLUTION FOUND BY OTHER THREAD, I AM STOPPING!");
+            }
+            
+        }
+        
+        Console.WriteLine($"{Thread.CurrentThread.Name}: COMPLETE");
+    }
+
+    /// <summary>
+    /// Returns true when the search value is greater than the
+    /// <see cref="StopWhenRandomValueIsGreaterThan"/> search stop threshold.
+    /// </summary>
+    /// <param name="searchValue"></param>
+    /// <returns></returns>
+    private static bool IsSearchValueFound(double searchValue) 
+        => searchValue > StopWhenRandomValueIsGreaterThan;
+}
---
+++ FlowFinalNode/readme.md
@@ -0,0 +1,64 @@
+﻿# FlowFinalNode Demo
+
+This small C# console application demonstrates converting the following activity diagram into threaded code.
+
+![diagram.png](diagram.png)
+
+## To Execute
+
+1. If needed, install the dotnet framework: https://dotnet.microsoft.com/download
+2. Open a terminal.
+3. Navigate to the ForkJoin project directory.
+4. Execute the following command: `dotnet run`
+
+## Output
+
+```csharp
+SOLUTION SEARCH ACTIVITY STARTING
+Post Problem Action
+FORKING 3 threads to search for solution
+3: Search Thread 3: STARTING, SEARCHING FOR A VALUE GREATER THAN 0.98
+1: Search Thread 1: STARTING, SEARCHING FOR A VALUE GREATER THAN 0.98
+2: Search Thread 2: STARTING, SEARCHING FOR A VALUE GREATER THAN 0.98
+2: Search Thread 2: Search iteration 1, found: 0.7786502887121685, :(
+3: Search Thread 3: Search iteration 1, found: 0.057402904737136495, :(
+1: Search Thread 1: Search iteration 1, found: 0.5208862541641001, :(
+2: Search Thread 2: Search iteration 2, found: 0.8453726510373731, :(
+1: Search Thread 1: Search iteration 2, found: 0.8188097772598439, :(
+3: Search Thread 3: Search iteration 2, found: 0.2457516867365528, :(
+2: Search Thread 2: Search iteration 3, found: 0.24723117044009624, :(
+3: Search Thread 3: Search iteration 3, found: 0.4604258725247352, :(
+1: Search Thread 1: Search iteration 3, found: 0.45920239397121243, :(
+2: Search Thread 2: Search iteration 4, found: 0.7295405690705945, :(
+3: Search Thread 3: Search iteration 4, found: 0.9283785532384793, :(
+1: Search Thread 1: Search iteration 4, found: 0.09274044799851033, :(
+2: Search Thread 2: Search iteration 5, found: 0.2001136773476493, :(
+1: Search Thread 1: Search iteration 5, found: 0.019400856537558075, :(
+3: Search Thread 3: Search iteration 5, found: 0.19075865783972257, :(
+3: Search Thread 3: Search iteration 6, found: 0.11280188897829357, :(
+2: Search Thread 2: Search iteration 6, found: 0.37288848802911323, :(
+1: Search Thread 1: Search iteration 6, found: 0.7647365395522935, :(
+3: Search Thread 3: Search iteration 7, found: 0.05807594331930066, :(
+2: Search Thread 2: Search iteration 7, found: 0.7913239549586616, :(
+2: Search Thread 2: Search iteration 8, found: 0.9179529804222597, :(
+1: Search Thread 1: Search iteration 7, found: 0.05722876723751624, :(
+3: Search Thread 3: Search iteration 8, found: 0.057724466888279924, :(
+2: Search Thread 2: Search iteration 9, found: 0.03327519025942682, :(
+1: Search Thread 1: Search iteration 8, found: 0.25176183452549783, :(
+3: Search Thread 3: Search iteration 9, found: 0.7683138022728685, :(
+2: Search Thread 2: Search iteration 10, found: 0.5443985368521728, :(
+1: Search Thread 1: Search iteration 9, found: 0.99651608723177, :)
+1: Search Thread 1: SOLUTION FOUND! 0.99651608723177, I WON! :)
+1: Search Thread 1: COMPLETE
+3: Search Thread 3: Search iteration 10, found: 0.27363166998929855, :(
+3: Search Thread 3: SOLUTION FOUND BY OTHER THREAD, I AM STOPPING!
+3: Search Thread 3: COMPLETE
+2: Search Thread 2: Search iteration 11, found: 0.9503919725414454, :(
+2: Search Thread 2: SOLUTION FOUND BY OTHER THREAD, I AM STOPPING!
+2: Search Thread 2: COMPLETE
+JOINED, all Search Threads are complete
+Select Solution Action: Search Value is: 0.99651608723177
+SOLUTION SEARCH ACTIVITY COMPLETE
+* Found in 30 total iterations over 3 threads.
+* Execution time: 00:00:06.8249554
+```
